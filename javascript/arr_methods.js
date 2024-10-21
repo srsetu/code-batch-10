0ce012e55ab0df7ebb05145151c55c55d04be96e
Saidur Rahman Setu <srsetu@gmail.com>
2024-10-22 01:32:59 +0600

    Reduce Discussion

--- javascript/arr_methods.js
+++ javascript/arr_methods.js
@@ -151,16 +151,124 @@
 
 // console.log(isEven);
 
-let nums = [12, 12, 234, 45, 232, 452, 123, 52, 3, 23];
+// let nums = [12, 12, 234, 45, 232, 452, 123, 52, 3, 23];
 
-nums.sort(function (a, b) {
-    return b - a;
-});
+// nums.sort(function (a, b) {
+//     return b - a;
+// });
 
 // negative -> a age boshe, b pore boshe
 // zero -> jemon ache temon e thakbe
 // positive -> b age boshe, a pore boshe
 
-console.log(nums);
+// console.log(nums);
 
 // https://github.com/jamesqquick/javascript-array-functions-practice
+
+// let nums = [1, 2, 3, 4, 5];
+// let result = 0;
+
+// for (let i = 0; i < nums.length; i++) {
+//     result += nums[i];
+// }
+
+// console.log(result);
+
+// let result = nums.reduce(function (acc, element) {
+//     return (acc += element);
+// }, 10);
+
+// console.log(result);
+
+// const characters = [
+//     {
+//         name: 'Luke Skywalker',
+//         height: '172',
+//         mass: '77',
+//         eye_color: 'blue',
+//         gender: 'male',
+//     },
+//     {
+//         name: 'Darth Vader',
+//         height: '202',
+//         mass: '136',
+//         eye_color: 'yellow',
+//         gender: 'male',
+//     },
+//     {
+//         name: 'Leia Organa',
+//         height: '150',
+//         mass: '49',
+//         eye_color: 'brown',
+//         gender: 'female',
+//     },
+//     {
+//         name: 'Anakin Skywalker',
+//         height: '188',
+//         mass: '84',
+//         eye_color: 'blue',
+//         gender: 'male',
+//     },
+// ];
+
+// Get the total mass of all characters
+// let totalMass = characters.reduce(function (acc, character) {
+//     return (acc += Number(character.mass));
+// }, 0);
+
+// console.log(totalMass);
+
+// Get the total height of all characters
+// Get the total number of characters in all the character names
+// let totalChar = characters.reduce(function (acc, character) {
+//     return (acc += character.name.length);
+// }, 0);
+
+// console.log(totalChar);
+
+// Get the total number of characters by eye color (hint. a map of eye color to count)
+
+// let charByEyeColor = characters.reduce(function (acc, character) {
+//     if (acc[character.eye_color]) {
+//         acc[character.eye_color]++;
+//     } else {
+//         acc[character.eye_color] = 1;
+//     }
+//     return acc;
+// }, {});
+
+// console.log(charByEyeColor);
+
+// acc.blue = 1;
+
+// {
+//     blue: 2;
+// }
+
+// character.eye_color
+// character['eye_color']
+
+// let nums = [1, 2, 3];
+
+// let result = nums.reduce(function (acc, el) {
+//     return acc;
+// }, {});
+
+// console.log(result);
+
+// let str = 'Hello World';
+
+// let result = str.split('').reduce(function (acc, val) {
+//     if (val != ' ') {
+//         if (acc[val]) {
+//             acc[val]++;
+//         } else {
+//             acc[val] = 1;
+//         }
+//     }
+//     return acc;
+// }, {});
+
+// console.log(result);
+
+// reducer(arr, initialval)
